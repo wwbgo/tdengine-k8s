2950db134202f4ab1011727a87847fd9ac23e8d3
wwbgo <wwbgo@qq.com>
2021-06-08 15:34:45 +0800

    2.1.2.0

--- docker/dockerbuild.sh
+++ docker/dockerbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-imageVer=2.1.1.2
-ver=2.1.1.0
+imageVer=2.1.2.0
+ver=2.1.2.0
 docker build --rm -f Dockerfile --build-arg ver=${ver} -t docker.flexem.com/library/tdengine:$imageVer .
 docker push docker.flexem.com/library/tdengine:$imageVer

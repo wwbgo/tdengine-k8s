da34e8d5c54805d77176b3912fb98d88b3f44b72
wwbgo <wwbgo@qq.com>
2021-12-09 11:09:19 +0800

    2.2.2.0

--- docker/dockerbuild.sh
+++ docker/dockerbuild.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-imageVer=2.2.0.5
-ver=2.2.0.5
+imageVer=2.2.2.0
+ver=2.2.2.0
 docker build --rm -f Dockerfile --build-arg ver=${ver} -t wwbgo/tdengine:$imageVer .
 docker push wwbgo/tdengine:$imageVer
